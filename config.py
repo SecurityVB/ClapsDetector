79b1a63891a06378d362cac6757a354c908f8d69
SecurityVB <koworking9992@gmail.com>
2026-01-10 12:29:54 +0300

    initProject

---
+++ config.py
@@ -0,0 +1,2 @@
+path_m4a = "Claps/Data_m4a/"
+path_wav = "Claps/Data_wav/"
---
+++ main.py
@@ -0,0 +1,48 @@
+# import librosa
+# import numpy as np
+from pydub import AudioSegment
+import os
+
+from GenerateDataSet import *
+from config import *
+
+
+# SR = 22050
+# N_FFT = 2048
+# HOP = 512
+# DURATION = 1.0
+#
+# def audio_to_spectrogram(path):
+#     y, sr = librosa.load(path, sr=SR)
+#
+#     target_len = int(SR * DURATION)
+#
+#     # жёстко приводим к 1 секунде
+#     if len(y) > target_len:
+#         y = y[:target_len]
+#     elif len(y) < target_len:
+#         y = np.pad(y, (0, target_len - len(y)))
+#
+#     S = np.abs(librosa.stft(
+#         y,
+#         n_fft=N_FFT,
+#         hop_length=HOP
+#     ))
+#
+#     spec = librosa.amplitude_to_db(S, ref=np.max)
+#
+#     # нормализация — ОБЯЗАТЕЛЬНО
+#     spec = (spec - spec.mean()) / spec.std()
+#
+#     return spec
+
+for file in os.listdir(path_m4a):
+    if file.endswith("m4a"):
+        audio = AudioSegment.from_file(path_m4a+file, format="m4a")
+        for db in range(-16, 17, 2):
+            if db != 0:
+                for i in (0.01, 0.02, 0.03):
+                    aug_audio = change_volume(add_noise(audio, i), i)
+                    save(f'Noisy_{i}_db_{db}.wav', aug_audio)
+
+# print(audio_to_spectrogram('Clap2.wav').shape)
